2f2e1b795c57f3b0b29e574631ada0271c334446
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2026-08-13 06:35:14 +0800

    done with configuring ws for prod

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -31,6 +31,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+print("DEBUG ALLOWED_HOSTS:", repr(ALLOWED_HOSTS))
 
 # Application definition
 
